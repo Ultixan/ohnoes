31f23bb0868c820ec6e40a1d383765c024773688
Ultixan <ultixan@gmail.com>
2013-01-27 14:12:26 +0200

    new instructions page

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    $('#next').click(function(ev) {
+    var clickFn;
+    var click2 = function(ev) {
         $('#instructions').attr('src', '/images/buttonrules.png');
-        $(this).hide();
+        $('#next').hide();
+    };
+    var click1 = function(ev) {
+        $('#instructions').attr('src', '/images/items.png');
+        clickFn = click2;
+    };
+    clickFn = click1;
+    $('#next').click(function(ev) {
+        clickFn(ev);
     });
     $('#play').click(function(ev) {
         window.location = '/game';
